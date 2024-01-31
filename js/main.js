792f8fdaab45d1d181599ab37006819d8dcc3d46
Shinya Ishizaki <shinya.ishizaki.0311@gmail.com>
2024-01-31 20:31:35 +0900

    creating a three points menu and  a login form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 {
+  function login() {
+    console.log("***Function login start***");
+  }
+
+  const menuLogin = document.getElementById("login");
+  const userId = document.getElementById("userid");
+  const userPassword = document.getElementById("userpassword");
   const menuItems = document.querySelectorAll('.menu li a');
   const contents = document.querySelectorAll('.content');
+
+  // ログインボタンをクリックしたとき、確認画面に遷移する
+  menuLogin.addEventListener("click", () => {
+    console.log("Login clicked.");
+    document.location.assign("confirm_login.html");
+  });
 
   menuItems.forEach(clickedItem => {
     clickedItem.addEventListener('click', e => {
