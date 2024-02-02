df9170d34700e44bc7a87cc2a1e0480182dcf646
Shinya Ishizaki <shinya.ishizaki.0311@gmail.com>
2024-02-02 17:18:35 +0900

    making a threepointleader and login confirm

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@
 {
   function login() {
     console.log("***Function login start***");
+    
+    const menuLogin = document.getElementById("login");
+    const userId = document.getElementById("userid");
+    const userPassword = document.getElementById("userpassword");
+    
+    // // ログインボタンをクリックしたとき、確認画面に遷移する
+    // menuLogin.addEventListener("click", () => {
+    //   console.log("Login clicked.");
+    //   document.location.assign("confirm_login.html");
+    // });
   }
-
-  const menuLogin = document.getElementById("login");
-  const userId = document.getElementById("userid");
-  const userPassword = document.getElementById("userpassword");
+  
   const menuItems = document.querySelectorAll('.menu li a');
   const contents = document.querySelectorAll('.content');
-
-  // ログインボタンをクリックしたとき、確認画面に遷移する
-  menuLogin.addEventListener("click", () => {
-    console.log("Login clicked.");
-    document.location.assign("confirm_login.html");
-  });
-
   menuItems.forEach(clickedItem => {
     clickedItem.addEventListener('click', e => {
       e.preventDefault();
-
+        
       menuItems.forEach(item => {
         item.classList.remove('active');
       });
       clickedItem.classList.add('active');
-      
+        
       contents.forEach(content => {
         content.classList.remove('active');
       });
@@ -33,4 +33,20 @@
     });
   });
 
+  // loginメソッドを呼び出す
+  const submitConfirm =  document.getElementById("submitConfirm");
+  submitConfirm.addEventListener("click", login);
+
+  const threePointLeaderId = document.getElementById("threePointLeaderId");
+  threePointLeaderId.addEventListener("click", () => {
+    console.log("threePointLeaderId clicked");
+    const threePointLeaderClass = document.getElementsByClassName("threePointLeader");
+    const userId = document.getElementById("userId");
+    const userPassword = document.getElementById("userPassword");
+    const loginSubmit = document.getElementById("loginSubmit");
+    threePointLeaderClass.classList.toggle("visible");
+    userId.classList.toggle("visible");
+    userPassword.classList.toggle("visible");
+    loginSubmit.classList.toggle("visible");
+  });
 }
